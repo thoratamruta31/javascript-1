03e3e6d8994266210583cb4ad3d7527448168fd8
thoratamruta31 <thoratamruta31@gmail.com>
2023-12-27 00:01:01 +0530

    Variable assignment_01 for JS
    Variable assignment_01 for JS

---
+++ 01_variableAssignment.js
@@ -0,0 +1,38 @@
+
+console.log("1.<-------------Before initialization-------------->");
+var myFullName;
+console.log(myFullName);
+
+console.log("<-------------After initialization with full name-------------->");
+myFullName="Amruta Thorat";
+console.log(myFullName);
+
+console.log("<-------------After updating with middle name-------------->");
+myFullName="Amruta Tushar Thorat";
+console.log(myFullName);
+
+console.log("2.<-------------Declaration and initialization-------------->");
+var myLove="I love only JS";
+console.log(myLove);
+
+console.log("3.<------------Declaration and initialization--------------->");
+var firstName="Amruta";
+console.log("First Name:",firstName);
+
+var lastName="Thorat";
+console.log("Last Name:",lastName);
+
+var age="32";
+console.log("Age:",age);
+
+var address="Pune";
+console.log("Address:",address);
+
+var pinCode="411033";
+console.log("Pincode:",pinCode);
+
+var gender="Female";
+console.log("Gender:",gender);
+
+var isMarried="Yes";
+console.log("Is married:",isMarried);
