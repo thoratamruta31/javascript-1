1ebded47fe327d616c1e59182c1245bc13cd729d
thoratamruta31 <thoratamruta31@gmail.com>
2023-12-27 00:03:08 +0530

    New file created for learning JS
    New file created for learning JS

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+console.log("Hello JS, I am Amruta");
+var num;
+console.log(num);
+
+var num = "10";
+console.log(num);
+
+var myName = "Amruta";
+console.log(myName);
+
+console.log("First Name:", myName);
+
+myName="Amruta Thorat";
+console.log( "Full Name:", myName);
