1c64fa4c5cc24c6b9953c1f9173ddd98a9263f26
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-03 00:20:40 +0530

    Updated for code prettier
    Updated for code prettier

--- 01_variableAssignment.js
+++ 01_variableAssignment.js
@@ -1,38 +1,37 @@
-
 console.log("1.<-------------Before initialization-------------->");
 var myFullName;
 console.log(myFullName);
 
 console.log("<-------------After initialization with full name-------------->");
-myFullName="Amruta Thorat";
+myFullName = "Amruta Thorat";
 console.log(myFullName);
 
 console.log("<-------------After updating with middle name-------------->");
-myFullName="Amruta Tushar Thorat";
+myFullName = "Amruta Tushar Thorat";
 console.log(myFullName);
 
 console.log("2.<-------------Declaration and initialization-------------->");
-var myLove="I love only JS";
+var myLove = "I love only JS";
 console.log(myLove);
 
 console.log("3.<------------Declaration and initialization--------------->");
-var firstName="Amruta";
-console.log("First Name:",firstName);
+var firstName = "Amruta";
+console.log("First Name:", firstName);
 
-var lastName="Thorat";
-console.log("Last Name:",lastName);
+var lastName = "Thorat";
+console.log("Last Name:", lastName);
 
-var age="32";
-console.log("Age:",age);
+var age = "32";
+console.log("Age:", age);
 
-var address="Pune";
-console.log("Address:",address);
+var address = "Pune";
+console.log("Address:", address);
 
-var pinCode="411033";
-console.log("Pincode:",pinCode);
+var pinCode = "411033";
+console.log("Pincode:", pinCode);
 
-var gender="Female";
-console.log("Gender:",gender);
+var gender = "Female";
+console.log("Gender:", gender);
 
-var isMarried="Yes";
-console.log("Is married:",isMarried);
+var isMarried = "Yes";
+console.log("Is married:", isMarried);
