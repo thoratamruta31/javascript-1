400219f19cb23dc5ac1ba044ffdf15ef6340982c
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-03 00:16:59 +0530

    Create 02_variableBasicsAssig.js
    Create 02_variableBasicsAssig.js

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,55 @@
+console.log(
+  "================== 3 Variables for string data type ======================="
+);
+var firstName = "Amruta";
+console.log("First name:", firstName, ",Data type:", typeof firstName);
+
+var gender = "Female";
+console.log("Gender:", gender, ",Data type:", typeof gender);
+
+var designation = "Project Officer";
+console.log("Designation:", designation, ",Data type:", typeof designation);
+
+console.log(
+  "================== 3 Variables for number data type ======================="
+);
+var employeeId = 767;
+console.log("Employee Id:", employeeId, ",Data type:", typeof employeeId);
+
+var totalMarks = 750;
+console.log("Total Marks:", totalMarks, ",Data type:", typeof totalMarks);
+
+var countOfAssign = 29;
+console.log(
+  "Total count of assignment:",
+  countOfAssign,
+  ",Data type:",
+  typeof countOfAssign
+);
+
+console.log(
+  "================== 3 Variables for boolean data type ======================="
+);
+var isCapable = true;
+console.log("Is Capable:", isCapable, ",Data type:", typeof isCapable);
+
+var isResponsible = true;
+console.log(
+  "Is Responsible:",
+  isResponsible,
+  ",Data type:",
+  typeof isResponsible
+);
+
+var isPostGraduated = false;
+console.log(
+  "Is Post Graduated:",
+  isPostGraduated,
+  ",Data type:",
+  typeof isPostGraduated
+);
+
+console.log("================== Null vs Undefined =======================");
+console.log(
+  "When declared the variable and initialize with the value of null it's null ; variable is declared but does not initialize with any value and this value is undefined "
+);
