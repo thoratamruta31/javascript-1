c25b592b179cd5ae29a082fc1b21af7fec8c1e7d
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-03 00:39:15 +0530

    Create 02_typesOf.js
    Create 02_typesOf.js

---
+++ 02_typeOf.js
@@ -0,0 +1,21 @@
+console.log("================= step 1 ======================");
+var bankName;
+console.log(bankName, ",Type of datatype:", typeof bankName);
+bankName = "SBI Bank";
+console.log("Bank Name:", bankName, ",Type of datatype:", typeof bankName);
+
+console.log("================= step 2 ======================");
+var marks = "90%";
+console.log("Marks:", marks, ",Type of datatype:", typeof marks);
+
+console.log("================= step 3 ======================");
+var isWorking;
+console.log(isWorking, ",Type of datatype:", typeof isWorking);
+isWorking = true;
+console.log("Is Working:", isWorking, ",Type of datatype:", typeof isWorking);
+
+console.log("================= step 4 ======================");
+var totalCount = "Hundred and Seven";
+console.log("Total Count:", totalCount,",Type of datatype:",typeof totalCount);
+totalCount = 107;
+console.log("Total Count:",totalCount,",Type of datatype:",typeof totalCount);
