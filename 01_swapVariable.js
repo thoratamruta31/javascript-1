e61ad3f42ee2e711f11867a93e85b26393de7eea
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-03 01:27:38 +0530

    create assignment for learning swap
    create assignment for learning swap

---
+++ 01_swapVariable.js
@@ -0,0 +1,7 @@
+var n1 = "10";
+var n2 = "20";
+console.log("before swap: n1=", n1, ",n2=", n2);
+var temp = n1;
+n1 = n2;
+n2 = temp;
+console.log("After swap: n1=", n1, ",n2=", n2);
---
+++ 02_swapVariables.js
@@ -0,0 +1,19 @@
+console.log("============  Step 1 ============");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Before swap: sweety =", sweety, ",cutie =", cutie);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("After swap: sweety =", sweety, ",cutie =", cutie);
+
+console.log("============  Step 2 ============");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("Before swap: num1 =", num1, ",num2 =", num2, ",num3 =", num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("After swap: num1 =", num1, ",num2 =", num2, ",num3 =", num3);
