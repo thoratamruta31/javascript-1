948b3dbaf008c760637c910e34eecdc9de3efabf
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-03 00:21:57 +0530

    Create file for learning datatype
    Create file for learning datatype

---
+++ 02_datatype.js
@@ -0,0 +1,19 @@
+var firstName = "Amruta";
+console.log(typeof firstName);
+
+var age = 32;
+var result = typeof age;
+console.log("Type of age:", result);
+
+var isMarried = true;
+console.log(typeof isMarried);
+var result = typeof isMarried;
+console.log("Type of isMarried:", result);
+
+var city;
+console.log(city);
+console.log("Type of city:", city);
+
+var country= null;
+console.log(country);
+console.log("Type of country:",typeof country);
