44e1fe5d2c4278ed499c6022ea8316660a38a8cc
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-04 01:21:47 +0530

    updated 03_string.js
    updated 03_string.js

--- 03_string.js
+++ 03_string.js
@@ -47,3 +47,41 @@ console.log(city.length);
 var trimResult = city.trim();
 
 console.log( ` After trim city is: ${trimResult}, It's length is: ${trimResult.length} `);
+
+var num = 15;
+
+var result = num.toString();
+
+console.log(` Number is ${num} and it's type is ${typeof num} `);
+
+console.log(` After conversion number is ${result}, it's type is ${typeof result}`);
+
+var greet = "Good Morning";
+
+var isAvailable = greet.includes("ing");
+
+console.log(` Is substring "ing" available => ${isAvailable}`);
+var result = greet.slice(5, 10);
+
+console.log(` Slice is ${result}  `);
+
+ 
+
+var word = "Jenny Herry";
+
+var result = word.split(" ");
+
+console.log(result);
+
+console.log(` Total words in the string is: ${result.length} `);
+
+console.log(typeof result);
+
+
+var givenString = "Hey I am UI and Java Developer";
+
+var result = givenString.split(" ");
+
+console.log(result);
+
+console.log(`Total words in string is: ${result.length}`);
