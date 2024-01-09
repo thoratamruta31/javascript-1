d72e31ab24b27d6e0209c8cb2f6799c8ebc42efa
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-09 13:54:37 +0530

    08_spreadOperator.js
    08_spreadOperator.js

---
+++ 08_spreadOperator.js
@@ -0,0 +1,8 @@
+let array = ["Jenny", "Menny", "Bill"];
+
+// Spread Operator ...
+console.log(array); // ['Jenny', 'Menny', 'Bill']
+console.log(...array); // Jenny Menny Bill
+
+const newArray =[...array];
+console.log(newArray);
