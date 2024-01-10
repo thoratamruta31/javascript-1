3486b3e2af557c5943741f76586fa4969981b31f
thoratamruta31 <thoratamruta31@gmail.com>
2024-01-10 10:41:36 +0530

    updated file
    updated file

--- 09_objectAssigD.js
+++ 09_objectAssigD.js
@@ -1,18 +1,22 @@
 let professor = {
-    name:"S.B.Patil",
-    age: 43,
-    designation: "Head of Department",
-    subject:"zoology",
-    address:"Rajgurunagar",
-    degrees:{
-        engineering:"CSC",
-        PHD:"Adv computing",
-        LLB:"Human Rights & International Law",
-        Msc:"Zoology"
-    } ,  
-    certificates: ["Hacker rank participation","Certificate  in IFE course","Certificate in Adv programming"],
-    totalExp: "14 years",
-}
+  name: "S.B.Patil",
+  age: 43,
+  designation: "Head of Department",
+  subject: "zoology",
+  address: "Rajgurunagar",
+  degrees: {
+    engineering: "CSC",
+    PHD: "Adv computing",
+    LLB: "Human Rights & International Law",
+    Msc: "Zoology",
+  },
+  certificates: [
+    "Hacker rank participation",
+    "Certificate  in IFE course",
+    "Certificate in Adv programming",
+  ],
+  totalExp: "14 years",
+};
 
 console.log("=========== Step 1 =============");
 console.log(professor);
@@ -32,26 +36,25 @@ professor.address = "Pune";
 console.log(`Modified address by pune: ${professor.address}`);
 
 console.log("=========== Step 6 =============");
-professor.certificates.splice(2,0,"Oracle  certified");
-console.log( "Updated Certificates for oracle certified: ",professor.certificates);
+professor.certificates.splice(2, 0, "Oracle  certified");
+console.log(
+  "Updated Certificates for oracle certified: ",
+  professor.certificates
+);
 
 console.log("=========== Step 7 =============");
 let arrayLength = professor.certificates.length;
-console.log("Last element of Certicates array is:",professor.certificates[arrayLength-1]);
+console.log(
+  "Last element of Certicates array is:",
+  professor.certificates[arrayLength - 1]
+);
 
 console.log("=========== Step 8 =============");
 console.log(professor);
 
-console.log("=========== Step 9 (traversing array using for of loop) =============");
+console.log(
+  "=========== Step 9 (traversing array using for of loop) ============="
+);
 for (const element of professor.certificates) {
-    console.log(element);
+  console.log(element);
 }
-
-
-
-
-
-
-
-
-
